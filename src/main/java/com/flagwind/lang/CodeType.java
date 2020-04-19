994cdad718e88190d664a10ece4f0b7f5f46a09f
chenabao <chendb1985@gamil.com>
2020-04-19 10:40:28 +0800

    [U]解决警告

--- src/main/java/com/flagwind/lang/CodeType.java
+++ src/main/java/com/flagwind/lang/CodeType.java
@@ -7,16 +7,12 @@ public interface CodeType {
     /**
      * 属性值
      * @return 属性值
-     * @author chendb
-     * @date 2016年12月8日 下午11:44:20
      */
     String getValue();
 
     /**
      * 属性描述信息
      * @return 描述信息
-     * @author chendb
-     * @date 2016年12月8日 下午11:44:42
      */
     String getText();
 }
